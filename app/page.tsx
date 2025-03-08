99d1774e70cc875288dc8ff3b895d40d4d8642df
Christober1010 <christoebredward@gmail.com>
2025-03-08 19:16:39 +0530

    reduxed react from 19 to 18

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link"
-
 import { Button } from "@/components/ui/button"
 import FeaturedProducts from "@/components/featured-products"
 import HeroSection from "@/components/hero-section"
